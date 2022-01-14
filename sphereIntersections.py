e43fb50e12dbbe6b2a61934cf45e98887738d0bb
IsaacLizardKing <joshfavorite03@gmail.com>
2022-01-13 20:07:54 -0600

    Renders now

--- sphereIntersections.py
+++ sphereIntersections.py
@@ -25,26 +25,50 @@ class camera:
 	def __init__(self, theta, phi, fov, width, height, x, y, z):
 		rays = np.zeros((h, w, 5), np.float32)
 		Y, X = np.mgrid[:height, :width]
-		pAdj = (Y - ((Y.shape[0] - 1) / 2))
-		tAdj = (X - ((X.shape[1] - 1) / 2))
-		maxAdj = max(np.max(np.absolute(pAdj)), np.max(np.absolute(tAdj)))
-		pAdj /= maxAdj
-		pAdj *= (fov / 2)
-		tAdj /= maxAdj
-		tAdj *= (fov / 2)
+		coords = np.zeros((h, w, 3), np.float32)
+		X = (X - ((X.shape[1] - 1) / 2))
+		Y = (Y - ((Y.shape[0] - 1) / 2))
+		# ~ print(X)
+		
+		coords[:,:,0] = X[:,:]
+		coords[:,:,1] = Y[:,:]
+		coords[:,:,2] = spherical2Cartesian(fov / 2, fov / 2, cartesian2Spherical(X[0, -1], Y[-1, 0], math.sqrt(((width / 2) ** 2 + (height / 2) ** 2)))[2])[2]
+
+		R1 = np.array([[math.cos(theta), -math.sin(theta),  0], [math.sin(theta), math.cos(theta), 0], [0, 0, 1]], np.float32)
+		R2 = np.array([[math.cos(phi), 0, math.sin(phi)], [0, 1, 0], [-math.sin(phi), 0, math.cos(phi)]], np.float32)
+		Matrix = R1@R2
+		# ~ print(Matrix)
+		coords[:,:,0] = coords[:, :, 0] * Matrix[0, 0] + coords[:, :, 1] * Matrix[0, 1] + coords[:, :, 2] * Matrix[0, 2]
+		coords[:,:,1] = coords[:, :, 0] * Matrix[1, 0] + coords[:, :, 1] * Matrix[1, 1] + coords[:, :, 2] * Matrix[1, 2]
+		coords[:,:,2] = coords[:, :, 0] * Matrix[2, 0] + coords[:, :, 1] * Matrix[2, 1] + coords[:, :, 2] * Matrix[2, 2]
 		rays[:,:,0] = x
 		rays[:,:,1] = y
 		rays[:,:,2] = z
-		rays[:,:,3], rays[:,:,4] = tAdj + theta, pAdj + phi
-		print(f"{np.max(rays[:,:,3])}, {rays[rays.shape[0] // 2, rays.shape[1] // 2, 3]}, {np.min(rays[:,:,3])}")
-		print(f"{np.max(rays[:,:,4])}, {rays[rays.shape[0] // 2, rays.shape[1] // 2, 4]}, {np.min(rays[:,:,4])}")
-		
+		t, p, r = cartesian2Spherical(coords[:,:,0], coords[:,:,1], coords[:,:,2])
+		rays[:,:,3], rays[:,:,4] = t, p
+		# ~ print(spherical2Cartesian(t, p, r))
 		self.rays = rays
-		
-		
-		
+	def RenderWorld(Spheres):
+		r = Sphere[3]
+		Sphere = np.array(Sphere, np.int32)[:3]
+		vect = spherical2Cartesian(Ray[3], Ray[4], 1)s
+		vect /= np.max(vect)
+		maxindex = np.argmax(vect)
+		cord = (Ray[:3] - Ray[maxindex]) / vect[maxindex]
+		a = np.sum(vect ** 2)
+		b = np.sum(2 * (cord[:] - Sphere[:]) * vect[:])
+		c = np.sum((cord - Sphere) ** 2) - r ** 2
+		if(b**2 - 4 * a * c < 0):
+			return [0, 0, 0]
+		ans = (-b + Op.pM(math.sqrt(b**2 - (4 * a * c)))) / (2 * a)
+		X = ans * vect[0]
+		Y = ans * vect[1]
+		Z = ans * vect[2]
+		inters = np.array([[X[d], Y[d], Z[d]] for d in range(0, X.shape[0])], np.float32)
 
-
+		
+		
+		
 class Op:
 	def pM(a):
 		return np.array([a, -a], np.float64)
@@ -68,32 +92,23 @@ def findCircleIntersection(Circle, Ray):
 precisionLevel = 0xFFFFFFFF
 def truncateFloatPointError(a):
 	global precisionLevel
-	return np.int64(precisionLevel * a) / precisionLevel
+	return np.int32(precisionLevel * a) / precisionLevel
 
 
 def findSphereIntersection(Sphere, Ray):
-	global precisionLevel
 	r = Sphere[3]
-	Sphere = np.array(Sphere, np.float64)[:3]
-	vx = truncateFloatPointError(math.cos(Ray[3] / 180 * math.pi) * math.sin(Ray[4] / 180 * math.pi))
-	vy = truncateFloatPointError(math.sin(Ray[3] / 180 * math.pi) * math.sin(Ray[4] / 180 * math.pi))
-	vz = truncateFloatPointError(math.cos(Ray[4] / 180 * math.pi))
-	# ~ print(Ray[3], Ray[4])
-	vect = np.array([vx, vy, vz], np.float32)
+	Sphere = np.array(Sphere, np.int32)[:3]
+	vect = spherical2Cartesian(Ray[3], Ray[4], 1)
+	# ~ print(vect, Ray[3:])
 	vect /= np.max(vect)
-	
 	maxindex = np.argmax(vect)
-	
-	lxt = (Ray[0] - Ray[maxindex]) / vect[maxindex]
-	lyt = (Ray[1] - Ray[maxindex]) / vect[maxindex]
-	lzt = (Ray[2] - Ray[maxindex]) / vect[maxindex]
-	cord = np.array([lxt, lyt, lzt], np.float32)
-	# ~ print(vect)
+	cord = (Ray[:3] - Ray[maxindex]) / vect[maxindex]
 	a = np.sum(vect ** 2)
-	b = np.sum(2 * (cord - Sphere) * vect)
+	b = np.sum(2 * (cord[:] - Sphere[:]) * vect[:])
 	c = np.sum((cord - Sphere) ** 2) - r ** 2
 	
 	if(b**2 - 4 * a * c < 0):
+		# ~ print("youch")
 		return [0, 0, 0]
 	# ~ print("yippee!")
 	ans = (-b + Op.pM(math.sqrt(b**2 - (4 * a * c)))) / (2 * a)
@@ -105,11 +120,11 @@ def findSphereIntersection(Sphere, Ray):
 
 def calculateNormal(Sphere, Intersection, Ray):
 	if((Intersection[0] == 0 and Intersection[1] == 0 and Intersection[2] == 0)):
-		return spherical2Cartesian(Ray[3], Ray[4], 0)
+		return spherical2Cartesian(Ray[3], Ray[4], 1)
 		# ~ return (Ray[3], Ray[4])
 	# ~ print("aye carumba!")
 	theta, phi, _ = cartesian2Spherical((Intersection[0] - Sphere[0]), (Intersection[1] - Sphere[1]), (Intersection[2] - Sphere[2]))
-	return spherical2Cartesian(theta, phi, 1)
+	return spherical2Cartesian(theta, phi, -1)
 	# ~ return theta, phi
 	
 def Render(Cam, Sphere):
@@ -121,9 +136,8 @@ def Render(Cam, Sphere):
 	print(Cam.rays.shape)
 	print("normalized!")
 	x, y, z = spherical2Cartesian(Cam.rays[:,:,3], Cam.rays[:, :, 4], 1)
-	Rays = np.array([[[x[a, b], y[a, b], z[a, b]] for b in range(0, x.shape[1])] for a in range(0, x.shape[0])], np.float32)
 	Frame = np.zeros((Cam.rays.shape[0], Cam.rays.shape[1]), np.float32)
-	Frame[:,:] = truncateFloatPointError(Normals[:, :, 0] - Rays[:, :, 0] + Normals[:, :, 1] - Rays[:, :, 1] + Normals[:, :, 2] + Rays[:, :, 2])
+	Frame[:,:] = (Normals[:, :, 0] - x[:,:] + Normals[:, :, 1] - y[:, :] + Normals[:, :, 2] - z[:, :])
 	# ~ Frame[Normals[:,:,:] == [0, 0, 0]] = [0] 
 	print(np.max(Frame))
 	print(np.min(Frame))
@@ -139,16 +153,20 @@ def Render(Cam, Sphere):
 
 
 
-XT = 20
-YT = 10
-ZT = -12
-Sphere = np.array([20, 10, 1, 10], np.float32)
+XT = 0
+YT = 0
+ZT = 0
+Sphere = np.array([0, 0, 20, 10], np.float32)
 theta, phi, _ = cartesian2Spherical(Sphere[0] - XT, Sphere[1] - YT, Sphere[2] - ZT)
-print(theta, phi)
-fov = 90
+Ray = np.array([XT, YT, ZT, theta, phi], np.float32)
+# ~ print(findSphereIntersection(Sphere, Ray))
+# ~ print(theta, phi)
+fov = 100
 fov = fov / 180 * math.pi
-w = 500
-h = 500
+w = 200
+h = 200
 cam = camera(theta, phi, fov, w, h, XT, YT, ZT)
 # ~ light = [0, 0]
 show(Render(cam, Sphere))
+
+
